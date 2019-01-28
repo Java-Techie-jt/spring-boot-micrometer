d9b4791c6b54cd3793eee588d09ab1814341739b
Basant kumar Hota <basahota@users.noreply.github.com>
2019-01-28 22:40:50 +0530

    #code

---
+++ src/test/java/com/javatechie/micrometer/api/SpringBootMicrometerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.javatechie.micrometer.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMicrometerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
